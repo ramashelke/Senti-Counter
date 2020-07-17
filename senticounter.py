404d412e97d41efd90e60c1f51b8b02b5d0d791b
Rama Shelke <65683113+ramashelke@users.noreply.github.com>
2020-07-17 18:20:02 -0400

    Added code for positive lexicon

--- senticounter.py
+++ senticounter.py
@@ -17,11 +17,14 @@ def loadLexicon(fname):
 #function that reads in a file with reviews and decides if each review is positive or negative
 #The function returns a list of the input reviews and a list of the respective decisions
 def run(path):
-    freq={} 
-    w=0
+    negfreq={} 
+    posfreq={} 
+    negCounter=0
+    posCounter=0
     reviews=[]
     #load the positive and negative lexicons
     negLex=loadLexicon('negative-words.txt')
+    posLex=loadLexicon('positive-words.txt')
     fin=open(path)
     for line in fin: # for every line in the file (1 review per line)
         line=line.lower().strip() 
@@ -30,17 +33,25 @@ def run(path):
    
         for i in range(len(words)): #for every word in the review
             if words[i] in negLex: # if the word is in the negative lexicon
-                w=i+1
-                if w+1>len(words):
+                negCounter=i+1
+                if negCounter+1>len(words):
                     break
                 else:
                     next_word=words[i+1]
-                    freq[next_word]=freq.get(next_word,0)+1
+                    negfreq[next_word]=negfreq.get(next_word,0)+1
+            elif words[i] in posLex:
+                posCounter=i+1
+                if posCounter+1>len(words):
+                    break
+                else:
+                    next_word=words[i+1]
+                    posfreq[next_word]=posfreq.get(next_word,0)+1
+
             
             
    
     fin.close()
-    return freq
+    return negfreq,posfreq
 
 
 if __name__ == "__main__": 
